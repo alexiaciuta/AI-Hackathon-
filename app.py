40ddb13d68f7ef28605eb69e53abcd7b8b58de56
Kirsten <kikielse05@outlook.com>
2024-11-10 10:12:36 +0000

    Files to create and host the sign up webpage

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        age = request.form.get('age')
+        location = request.form.get('location')
+        interests = request.form.getlist('interests')
+        connection_type = request.form.get('connection_type')
+        # For now, we'll just display a thank you message with the user's name
+        return f"Thank you for signing up, {name}!"
+
+    interests_options = ['Music', 'Sports', 'Reading', 'Traveling', 'Gaming']
+    return render_template('signup.html', interests_options=interests_options)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
